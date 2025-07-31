274b0d4b85ca4a238f96ad7e87f52f714bfdea8a
viv <116047994+itsvivianmill@users.noreply.github.com>
2025-07-30 21:27:15 -0700

    Update quartz.config.ts

--- .github/quartz/quartz.config.ts
+++ .github/quartz/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Vivian's Journal",
+    pageTitle: "Vivian's Journal",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
